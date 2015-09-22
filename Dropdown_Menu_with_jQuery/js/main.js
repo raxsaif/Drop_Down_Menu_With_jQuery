4b480eba4f0e096442bf5bdb20db99b74a08de12
Saiful Saif <saif.galaxy@gmail.com>
2015-09-22 14:26:41 +0600

    Dropdown_menu_with_JQuery
    Dropdown_Menu_With_jQuery

---
+++ Dropdown_Menu_with_jQuery/js/main.js
@@ -0,0 +1,13 @@
+$(function(){
+  $('#navigation_horiz').naviDropDown({
+    dropDownClass: 'dropdown', //the class name for your drop down
+dropDownWidth: 'auto', //the default width of drop down elements
+slideDownEasing: 'easeInCirc', //easing method for slideDown
+slideUpEasing: 'easeInOutBounce', //easing method for slideUp
+slideDownDuration: 500, //easing duration for slideDown
+slideUpDuration: 500, //easing duration for slideUp
+orientation: 'horizontal' //orientation - either 'horizontal' or 'vertical'
+  });
+
+
+});
